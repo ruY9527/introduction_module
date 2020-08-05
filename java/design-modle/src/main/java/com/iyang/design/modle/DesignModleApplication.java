af82e92edf0f37067c574616bfdf1015692d2dbb
baoyang23 <1411091515@qq.com>
2020-08-06 00:33:43 +0800

    feat:提交Java类型的设计模式

---
+++ java/design-modle/src/main/java/com/iyang/design/modle/DesignModleApplication.java
@@ -0,0 +1,29 @@
+package com.iyang.design.modle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author yang
+ * Java 设计模式的基础 Coding 学习和总和运用使用.
+ *
+ * 在开发一个项目项目,我们都会弄清楚,开发的这个东西大致需求.
+ * 结合这些需求的话,我们就可以使用对应的设计模式来完成我们的代码功能.
+ *
+ * 1. 创建型模式
+ * 2. 结构性模式
+ * 3. 行为型模式
+ * 4. J2EE模式.
+ * 当然了,设计模式这个肯定会随着越来越多的东西,对应的使用也是会越来越多的.
+ * 主要是先学习理解这个设计模式的好处,然后在学习在工作中对其进行使用
+ *
+ */
+@SpringBootApplication
+public class DesignModleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModleApplication.class, args);
+    }
+
+}
---
+++ java/design-modle/src/test/java/com/iyang/design/modle/DesignModleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyang.design.modle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
