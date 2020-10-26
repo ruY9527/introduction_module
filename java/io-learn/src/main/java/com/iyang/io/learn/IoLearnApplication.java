bae32dabe4b4d20a7669e96e97079a1623e952ca
baoYang <1411091515@qq.com>
2020-10-26 22:22:14 +0800

    feat:提交IO模块学习

---
+++ java/io-learn/src/main/java/com/iyang/io/learn/IoLearnApplication.java
@@ -0,0 +1,13 @@
+package com.iyang.io.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoLearnApplication.class, args);
+    }
+
+}
